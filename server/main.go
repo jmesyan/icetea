115868d5e4a4b92e8576d326726333caa7fbc001
jmesyan <jieqiang1017@qq.com>
2018-08-12 07:31:19 +0800

    protobuffed

--- server/main.go
+++ server/main.go
@@ -1,140 +1,139 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "time"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
-    "github.com/jmesyan/nano"
-    "github.com/jmesyan/nano/component"
-    "github.com/jmesyan/nano/serialize/protobuf"
-    "github.com/jmesyan/nano/session"
-    "icetea/server/protos"
-    "strings"
+	"github.com/jmesyan/nano"
+	"github.com/jmesyan/nano/component"
+	"github.com/jmesyan/nano/serialize/protobuf"
+	"github.com/jmesyan/nano/session"
+	"icetea/server/protos"
+	"strings"
 )
 
-type ( 
-    Room struct {
-        group *nano.Group
-    }
+type (
+	Room struct {
+		group *nano.Group
+	}
 
-    // RoomManager represents a component that contains a bundle of room
-    RoomManager struct {
-        component.Base
-        timer *nano.Timer
-        rooms map[int]*Room
-    }
+	// RoomManager represents a component that contains a bundle of room
+	RoomManager struct {
+		component.Base
+		timer *nano.Timer
+		rooms map[int]*Room
+	}
 
-    stats struct {
-        component.Base
-        timer         *nano.Timer
-        outboundBytes int
-        inboundBytes  int
-    }
+	stats struct {
+		component.Base
+		timer         *nano.Timer
+		outboundBytes int
+		inboundBytes  int
+	}
 )
 
 func (stats *stats) outbound(s *session.Session, msg nano.Message) error {
-    stats.outboundBytes += len(msg.Data)
-    return nil
+	stats.outboundBytes += len(msg.Data)
+	return nil
 }
 
 func (stats *stats) inbound(s *session.Session, msg nano.Message) error {
-    stats.inboundBytes += len(msg.Data)
-    return nil
+	stats.inboundBytes += len(msg.Data)
+	return nil
 }
 
 func (stats *stats) AfterInit() {
-    stats.timer = nano.NewTimer(time.Minute, func() {
-        println("OutboundBytes", stats.outboundBytes)
-        println("InboundBytes", stats.outboundBytes)
-    })
+	stats.timer = nano.NewTimer(time.Minute, func() {
+		println("OutboundBytes", stats.outboundBytes)
+		println("InboundBytes", stats.outboundBytes)
+	})
 }
 
 const (
-    testRoomID = 1
-    roomIDKey  = "ROOM_ID"
+	testRoomID = 1
+	roomIDKey  = "ROOM_ID"
 )
 
 func NewRoomManager() *RoomManager {
-    return &RoomManager{
-        rooms: map[int]*Room{},
-    }
+	return &RoomManager{
+		rooms: map[int]*Room{},
+	}
 }
 
 // AfterInit component lifetime callback
 func (mgr *RoomManager) AfterInit() {
-    nano.OnSessionClosed(func(s *session.Session) {
-        if !s.HasKey(roomIDKey) {
-            return
-        }
-        room := s.Value(roomIDKey).(*Room)
-        room.group.Leave(s)
-    })
-    mgr.timer = nano.NewTimer(time.Minute, func() {
-        for roomId, room := range mgr.rooms {
-            println(fmt.Sprintf("UserCount: RoomID=%d, Time=%s, Count=%d",
-                roomId, time.Now().String(), room.group.Count()))
-        }
-    })
+	nano.OnSessionClosed(func(s *session.Session) {
+		if !s.HasKey(roomIDKey) {
+			return
+		}
+		room := s.Value(roomIDKey).(*Room)
+		room.group.Leave(s)
+	})
+	mgr.timer = nano.NewTimer(time.Minute, func() {
+		for roomId, room := range mgr.rooms {
+			println(fmt.Sprintf("UserCount: RoomID=%d, Time=%s, Count=%d",
+				roomId, time.Now().String(), room.group.Count()))
+		}
+	})
 }
 
 // Join room
 func (mgr *RoomManager) Join(s *session.Session, msg []byte) error {
-    // NOTE: join test room only in demo
-    room, found := mgr.rooms[testRoomID]
-    if !found {
-        room = &Room{
-            group: nano.NewGroup(fmt.Sprintf("room-%d", testRoomID)),
-        }
-        mgr.rooms[testRoomID] = room
-    }
+	// NOTE: join test room only in demo
+	room, found := mgr.rooms[testRoomID]
+	if !found {
+		room = &Room{
+			group: nano.NewGroup(fmt.Sprintf("room-%d", testRoomID)),
+		}
+		mgr.rooms[testRoomID] = room
+	}
 
-    fakeUID := s.ID() //just use s.ID as uid !!!
-    s.Bind(fakeUID)   // binding session uids.Set(roomIDKey, room)
-    s.Set(roomIDKey, room)
-    s.Push("onMembers", &protos.AllMembers{Members: room.group.Members()})
-    // notify others
-    room.group.Broadcast("onNewUser", &protos.NewUser{Content: fmt.Sprintf("New user: %d", s.ID())})
-    // new user join group
-    room.group.Add(s) // add session to group
-    return s.Response(&protos.JoinResponse{Result: "success"})
+	fakeUID := s.ID() //just use s.ID as uid !!!
+	s.Bind(fakeUID)   // binding session uids.Set(roomIDKey, room)
+	s.Set(roomIDKey, room)
+	s.Push("onMembers", &protos.AllMembers{Members: room.group.Members()})
+	// notify others
+	room.group.Broadcast("onNewUser", &protos.NewUser{Content: fmt.Sprintf("New user: %d", s.ID())})
+	// new user join group
+	room.group.Add(s) // add session to group
+	return s.Response(&protos.JoinResponse{Result: "success"})
 }
 
 // Message sync last message to all members
 func (mgr *RoomManager) Message(s *session.Session, msg *protos.UserMessage) error {
-    if !s.HasKey(roomIDKey) {
-        return fmt.Errorf("not join room yet")
-    }
-    room := s.Value(roomIDKey).(*Room)
-    return room.group.Broadcast("onMessage", msg)
+	if !s.HasKey(roomIDKey) {
+		return fmt.Errorf("not join room yet")
+	}
+	room := s.Value(roomIDKey).(*Room)
+	return room.group.Broadcast("onMessage", msg)
 }
 
 func main() {
-    // override default serializer
-    nano.SetSerializer(protobuf.NewSerializer())
-    nano.SetHeartbeatInterval(time.Duration(5) * time.Second)
+	// override default serializer
+	nano.SetSerializer(protobuf.NewSerializer())
+	nano.SetHeartbeatInterval(time.Duration(5) * time.Second)
 
-    // rewrite component and handler name
-    room := NewRoomManager()
-    nano.Register(room,
-        component.WithName("room"),
-        component.WithNameFunc(strings.ToLower),
-    )
+	// rewrite component and handler name
+	room := NewRoomManager()
+	nano.Register(room,
+		component.WithName("room"),
+		component.WithNameFunc(strings.ToLower),
+	)
 
-    // traffic stats
-    pipeline := nano.NewPipeline()
-    var stats = &stats{}
-    pipeline.Outbound().PushBack(stats.outbound)
-    pipeline.Inbound().PushBack(stats.inbound)
+	// traffic stats
+	pipeline := nano.NewPipeline()
+	var stats = &stats{}
+	pipeline.Outbound().PushBack(stats.outbound)
+	pipeline.Inbound().PushBack(stats.inbound)
 
-    nano.EnableDebug()
-    log.SetFlags(log.LstdFlags | log.Llongfile)
-    nano.SetWSPath("/nano")
+	nano.EnableDebug()
+	log.SetFlags(log.LstdFlags | log.Llongfile)
+	nano.SetWSPath("/nano")
 
-    http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
+	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
 
-    nano.SetCheckOriginFunc(func(_ *http.Request) bool { return true })
-    nano.Listen(":7873", nano.WithPipeline(pipeline))
+	nano.SetCheckOriginFunc(func(_ *http.Request) bool { return true })
+	nano.Listen(":7873", nano.WithPipeline(pipeline))
 }
-
